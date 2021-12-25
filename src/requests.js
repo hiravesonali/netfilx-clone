55793207a15487c73f12554ced69f47605957303
Sonali Hirave <sonali@wittypen.com>
2021-12-26 00:41:03 +0100

    deploy the netflix clone in firebase

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ const API_KEY = "7221b0e10a8ac6bcbee4eae9c5ed2f03";
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&langauge=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/movie/top_rated??api_key=${API_KEY}&languages=en-US`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&languages=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
