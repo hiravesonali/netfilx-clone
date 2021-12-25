7ef83775bd951fd5f0009db3c55bb6301ba53cb1
Sonali Hirave <sonali@wittypen.com>
2021-12-25 23:01:50 +0100

    add netlflix home page and banner

---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const API_KEY = "7221b0e10a8ac6bcbee4eae9c5ed2f03";
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&langauge=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated??api_key=${API_KEY}&languages=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaties: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
